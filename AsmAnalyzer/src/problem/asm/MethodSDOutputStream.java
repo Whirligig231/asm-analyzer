f756fedf3850e39fa139363f28b21aa215070bff
Stevenator1 <stevenkneisler@gmail.com>
2016-01-21 00:44:30 -0500

    Fixed MethodSDOutputStream

--- AsmAnalyzer/src/problem/asm/MethodSDOutputStream.java
+++ AsmAnalyzer/src/problem/asm/MethodSDOutputStream.java
@@ -3,11 +3,11 @@ package problem.asm;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
-import problem.asm.model.IClass;
 import problem.asm.model.IMethod;
-import problem.asm.model.IModel;
 import problem.asm.visitor.ITraverser;
 import problem.asm.visitor.IVisitMethod;
 import problem.asm.visitor.IVisitor;
@@ -17,17 +17,17 @@ import problem.asm.visitor.Visitor;
 public class MethodSDOutputStream extends FilterOutputStream {
 	private final IVisitor visitor;
 	private StringBuilder methodSection;
+	private Set<IMethod> visited; // This is to avoid endless loops otherwise caused by recursive functions
 	
 	public MethodSDOutputStream(OutputStream out) throws IOException {
 		super(out);
 		this.methodSection = new StringBuilder();
+		this.visited = new HashSet<IMethod>();
 		this.visitor = new Visitor();
-		this.setupPostVisitModel();
-		this.setupPreVisitClass();
 		this.setupVisitMethod();
 	}
 	
-	private void write(String m) {
+	public void write(String m) {
 		try {
 			super.write(m.getBytes());
 		}
@@ -41,45 +41,39 @@ public class MethodSDOutputStream extends FilterOutputStream {
 		t.accept(this.visitor);
 	}
 	
-	private void setupPreVisitClass() {
-		IVisitMethod command = new IVisitMethod() {
-			@Override
-			public void execute(ITraverser t) {
-				IClass c = (IClass)t;
-				String line = String.format("%s:%s[a]\n", c.getName(), c.getName());
-				write(line);
-			}
-		};
-		this.visitor.addVisit(VisitType.PreVisit, IClass.class, command);
-	}
-
 	private void setupVisitMethod() {
 		IVisitMethod command = new IVisitMethod() {
 			@Override
 			public void execute(ITraverser t) {
 				IMethod c = (IMethod)t;
+				if (visited.contains(c))
+					return;
+				else
+					visited.add(c);
+				
 				Iterator<IMethod> it = c.getCallIterator();
 				while (it.hasNext()) {
 					IMethod call = it.next();
-					String line = String.format("%s:%s.%s\n", c.getOwner().getName(), call.getOwner().getName(),
-							call.getName());
-					methodSection.append(line);
+					
+					//if (call.getName().equals("<init>")) {
+					//	this.write(String.format("%s:%s.new\n", c.getOwner().getName(), call.getOwner().getName()));
+					//}
+					//else {
+						String beginLine = String.format("%s:%s.%s(", c.getOwner().getName(), call.getOwner().getName(),
+								call.getName());
+						write(beginLine);
+						for (int i = 0; i < call.getArgTypes().length; i++) {
+							if (i > 0)
+								write(", ");
+							write(call.getArgTypes()[i]);
+						}
+						write(String.format(")\\: %s\n", call.getReturnType()));
+					//}
+		
+					call.accept(visitor);
 				}
 			}
 		};
 		this.visitor.addVisit(VisitType.Visit, IMethod.class, command);
 	}
-	
-	private void setupPostVisitModel() {
-		IVisitMethod command = new IVisitMethod() {
-			@Override
-			public void execute(ITraverser t) {
-				IModel c = (IModel)t;
-				write("\n");
-				write(methodSection.toString());
-			}
-		};
-		this.visitor.addVisit(VisitType.PostVisit, IModel.class, command);
-	}
-	
 }
