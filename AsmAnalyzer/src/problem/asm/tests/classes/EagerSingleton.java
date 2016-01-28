1d2e706d16ecd213f988deb258da317853efef3f
Stevenator1 <stevenkneisler@gmail.com>
2016-01-27 23:40:45 -0500

    Finished singleton testing

---
+++ AsmAnalyzer/src/problem/asm/tests/classes/EagerSingleton.java
@@ -0,0 +1,12 @@
+package problem.asm.tests.classes;
+
+public class EagerSingleton {
+	
+    private static final EagerSingleton INSTANCE = new EagerSingleton();
+
+    private EagerSingleton() {}
+
+    public static EagerSingleton getInstance() {
+        return INSTANCE;
+    }
+}
