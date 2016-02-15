f3490b3a17b74b0ca1b39595c1291171407b3d1d
Christian Schulz <schulzcc@rose-hulman.edu>
2016-02-14 22:03:57 -0500

    You can actually select and load a config file. It doesn't do anything yet.

--- AsmAnalyzer/src/analyzer/gui/LandingFrame.java
+++ AsmAnalyzer/src/analyzer/gui/LandingFrame.java
@@ -1,22 +1,36 @@
 package analyzer.gui;
 
 import java.awt.BorderLayout;
+import java.awt.TrayIcon.MessageType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class LandingFrame extends JFrame {
 
 	private static final long serialVersionUID = 322559617349772310L;
+	
+	private JButton analyze;
+	private Properties config;
+	private JFileChooser fc;
 	
 	public LandingFrame() {
 		this.setTitle("Design Parser");
@@ -35,9 +49,11 @@ public class LandingFrame extends JFrame {
 		top.add(loadConfig);
 		top.add(Box.createHorizontalStrut(10));
 		
-		JButton analyze = new JButton("Analyze");
-		top.add(analyze);
+		this.analyze = new JButton("Analyze");
+		top.add(this.analyze);
 		top.add(Box.createHorizontalGlue());
+		
+		this.analyze.setEnabled(false);
 		
 		main.add(top);
 		main.add(Box.createVerticalStrut(20));
@@ -67,12 +83,12 @@ public class LandingFrame extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(top, "Load the config? Noooooooooo!!!!!");
+				LandingFrame.this.getConfigFile();
 			}
 			
 		});
 		
-		analyze.addActionListener(new ActionListener() {
+		this.analyze.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -82,6 +98,42 @@ public class LandingFrame extends JFrame {
 			
 		});
 		
+		this.fc = new JFileChooser();
+		FileFilter filter = 
+				new FileNameExtensionFilter("Java Properties Files (.properties)", ".properties");
+		fc.addChoosableFileFilter(filter);
+		fc.setFileFilter(filter);
+		
+	}
+
+	private void getConfigFile() {
+		int returnVal = this.fc.showOpenDialog(this);
+		
+		if (returnVal == JFileChooser.APPROVE_OPTION) {	
+			File configFile = fc.getSelectedFile();
+			
+			try {
+				this.analyze.setEnabled(false);
+				this.loadConfig(configFile);
+				this.analyze.setEnabled(true);
+			} catch (FileNotFoundException e) {
+				this.error("could not open the config file");
+			} catch (IOException | IllegalArgumentException e) {
+				this.error("could not parse the config file");
+			}
+		}
+	}
+	
+	private void loadConfig(File configFile) throws IOException {
+		this.config = new Properties();
+		InputStream in = new FileInputStream(configFile);
+		this.config.load(in);
+		in.close();
+	}
+	
+	private void error(String errorMsg) {
+		JOptionPane.showMessageDialog(this, "Error: " + errorMsg, "An error occurred", 
+				JOptionPane.ERROR_MESSAGE);
 	}
 
 }
